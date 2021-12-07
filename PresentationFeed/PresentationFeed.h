cd3bdb9eb6091d0d423a698b19e37fbbb03e14b1
Alok <sishu.chandra18@gmail.com>
2021-12-07 21:38:40 +0100

    Initial Commit

---
+++ PresentationFeed/PresentationFeed.h
@@ -0,0 +1,18 @@
+//
+//  PresentationFeed.h
+//  PresentationFeed
+//
+//  Created by Alok Sinha on 2021-12-07.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PresentationFeed.
+FOUNDATION_EXPORT double PresentationFeedVersionNumber;
+
+//! Project version string for PresentationFeed.
+FOUNDATION_EXPORT const unsigned char PresentationFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PresentationFeed/PublicHeader.h>
+
+
